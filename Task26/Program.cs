6c9f3ea05b7edb464e050e73ace9d7e05c63fbc0
Maxim Prav <mxmlknv@mail.ru>
2023-02-14 16:48:20 +0300

    Задача с количеством цифр в числе

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе равняется {GetNums(num)}");
+
+
+int GetNums(int number)
+{
+    int count = 1;
+    while(number>10)
+    {
+        number = number/10;
+        count = count + 1; 
+    }
+    return count;
+}
