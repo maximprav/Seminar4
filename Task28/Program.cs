f68aa61cb0f060a845ffc34507183a48a7bb6959
Maxim Prav <mxmlknv@mail.ru>
2023-02-14 16:55:41 +0300

    Задача с произведением от 1 до N

---
+++ Task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифр = {GetPrd(num)}");
+
+int GetPrd(int number)
+{
+    int prd = 1;
+    while(number>1)
+    {
+        prd = prd*number;
+        number--;
+    }
+    return prd;
+}
