6032eebfd1c5db4c0e43d33fcba9762eb25ce926
Maxim Prav <mxmlknv@mail.ru>
2023-02-14 16:38:04 +0300

    Метод суммы чисел от 0 до N

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр равна {GetSumNums(num)}");
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number; // sum = sum + number
+        number--; // number = number - 1
+    }
+    return sum;
+}
