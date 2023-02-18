b15eabd74c17623d8a712a6c8a06b4c6d95ab418
Maxim Prav <mxmlknv@mail.ru>
2023-02-18 14:10:41 +0300

    Первая задача с массивом

---
+++ Task30/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+int[] array = new int[8];
+int index = 0;
+int size = array.Length;
+
+PrintArray(array);
+
+void PrintArray(int[] arr)
+{
+    while(index<size)
+    {
+        arr[index] = new Random().Next(0,2);
+        Console.Write(arr[index] + " ");
+        index++;
+    }
+}
+
+
