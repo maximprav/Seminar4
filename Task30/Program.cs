6c0525a4995593de14b1e2909dcfb758c7af0b3a
Maxim Prav <mxmlknv@mail.ru>
2023-02-18 14:28:46 +0300

    Доделана задача с массивом

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,22 +1,42 @@
 ﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
 // заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
+// Создать второй массив и сложить два массива
 
 Console.Clear();
-int[] array = new int[8];
-int index = 0;
-int size = array.Length;
+int[] arr = new int[8];
+int[] arr2 = new int[8];
+int[] arr3 = new int[8];
+RandomArray(arr);
+PrintArray(arr);
+Console.WriteLine(" ");
+RandomArray(arr2);
+PrintArray(arr2);
+Console.WriteLine(" ");
+SumArr(arr, arr2, arr3);
+PrintArray(arr3);
 
-PrintArray(array);
 
-void PrintArray(int[] arr)
+void PrintArray(int[] array)
 {
-    while(index<size)
-    {
-        arr[index] = new Random().Next(0,2);
-        Console.Write(arr[index] + " ");
-        index++;
-    }
+for (int i = 0; i < 8; i++)
+{
+Console.Write(array[i] + ", ");
+}
 }
 
+void RandomArray(int[] array)
+{
+for (int i = 0; i < 8; i++)
+{
+array[i] = new Random().Next(0, 2);
+}
+}
 
+void SumArr(int[] array1, int[] array2, int[] array3)
+{
+for (int i = 0; i < 8; i++)
+{
+array3[i] = array1[i] + array2[i];
+}
+}
